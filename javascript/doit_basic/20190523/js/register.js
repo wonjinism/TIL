5752b0b34bb5a27a9ab0ae97138db1b75096b03f
Wonjin Kim <wonjinism@gmail.com>
2019-05-23 22:34:36 +0900

    Do It Javascript Basic - DOM

---
+++ javascript/doit_basic/20190523/js/register.js
@@ -0,0 +1,25 @@
+function newRegister() {
+  var newP = document.createElement('p');
+  var userName = document.querySelector('#userName');
+  var newText = document.createTextNode(userName.value);
+  newP.appendChild(newText);
+
+  var delBtn = document.createElement('span');
+  var delText = document.createTextNode('X');
+  delBtn.setAttribute('class', 'del');
+  delBtn.appendChild(delText);
+  newP.appendChild(delBtn);
+
+  var nameList = document.querySelector('#nameList');
+  nameList.insertBefore(newP, nameList.childNodes[0]);
+  userName.value = '';
+
+  var removeBtns = document.querySelectorAll('.del');
+  for (let index = 0; index < removeBtns.length; index++) {
+    removeBtns[index].addEventListener('click', function(){
+      if(this.parentNode.parentNode) {
+        this.parentNode.parentNode.removeChild(this.parentNode);
+      }
+    });
+  }
+}

758574ceb594171a2b20a57eeb904c8af3f87e6f
Simon <wonjin@gmail.com>
2019-07-24 23:13:52 +0900

    Java 언어로 배우는 디자인 패턴 입문 - Iterator
    Chapter 01 Iterator 순서대로 지정해서 처리하기

---
+++ java/DesignPattern/EasyGoF/src/iterator/BookShelf.java
@@ -0,0 +1,23 @@
+package iterator;
+import java.util.ArrayList;
+
+public class BookShelf implements Aggregate {
+	private ArrayList<Book> books;
+	private int last = 0;
+	public BookShelf() {
+		this.books = new ArrayList<Book>();
+	}
+	public Book getBookAt(int index) {
+		return books.get(index);
+	}
+	public void appendBook(Book book) {
+		this.books.add(book);
+		last++;
+	}
+	public int getLength() {
+		return last;
+	}
+	public Iterator iterator() {
+		return new BookShelfIterator(this);
+	}
+}
